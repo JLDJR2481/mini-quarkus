799e9a2a704616242aaaf498473c1cec7860fe3b
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-05-14 19:11:25 +0200

    feat(): Marshalling JSON

--- getting-started/src/main/java/edu/craptocraft/quickstart/GreetingResource.java
+++ getting-started/src/main/java/edu/craptocraft/quickstart/GreetingResource.java
@@ -1,21 +1,27 @@
 package edu.craptocraft.quickstart;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @ConfigProperty(name = "greetings.message")
-    String msg;
-
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return msg;
+    @Produces(MediaType.APPLICATION_JSON)
+    public Beer getBeer() {
+        return new Beer("Alhambra", 300);
     }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createBeer(Beer beer) {
+        System.out.println(beer);
+        return Response.ok().build();
+    }
+
 }
